b1d189168ffd2b800d3257b7aeb23368c11f636e
Zdenek Dolezal <zdenek.dolezal@gmail.com>
2016-10-21 13:07:30 +0200

    Adjustments

--- src/main/java/com/jakewharton/fliptables/FlipTable.java
+++ src/main/java/com/jakewharton/fliptables/FlipTable.java
@@ -73,18 +73,20 @@ public final class FlipTable {
 
   @Override public String toString() {
     StringBuilder builder = new StringBuilder();
-    printDivider(builder, "╔═╤═╗");
+    printDivider(builder, "+-+-+");
     printData(builder, headers);
     if (data.length == 0) {
-      printDivider(builder, "╠═╧═╣");
-      builder.append('║').append(pad(emptyWidth, EMPTY)).append("║\n");
-      printDivider(builder, "╚═══╝");
+      printDivider(builder, "|-+-|");
+      builder.append('|').append(pad(emptyWidth, EMPTY)).append("|\n");
+      printDivider(builder, "+---+");
     } else {
       for (int row = 0; row < data.length; row++) {
-        printDivider(builder, row == 0 ? "╠═╪═╣" : "╟─┼─╢");
+        if (row == 0) {
+          printDivider(builder, row == 0 ? "|-|-|" : "|-|-|");
+        }
         printData(builder, data[row]);
       }
-      printDivider(builder, "╚═╧═╝");
+      printDivider(builder, "+-+-+");
     }
     return builder.toString();
   }
@@ -100,13 +102,13 @@ public final class FlipTable {
   private void printData(StringBuilder out, String[] data) {
     for (int line = 0, lines = 1; line < lines; line++) {
       for (int column = 0; column < columns; column++) {
-        out.append(column == 0 ? '║' : '│');
+        out.append(column == 0 ? '|' : '|');
         String[] cellLines = data[column].split("\\n");
         lines = Math.max(lines, cellLines.length);
         String cellLine = line < cellLines.length ? cellLines[line] : "";
         out.append(pad(columnWidths[column], cellLine));
       }
-      out.append("║\n");
+      out.append("|\n");
     }
   }
 
